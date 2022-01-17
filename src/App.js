57b74b6cc8bf804fd6411e8cb8c2cecb7b11d4f1
Danish-Ayub <67322647+Danish-Ayub@users.noreply.github.com>
2022-01-18 01:14:26 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './index.css';
+import React from "react";
+import "./styles/app.scss"
+
+// Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+// Util
+import Data from "./util";
 
 function App() {
   return (
     <div className="App">
-      <h1>Good</h1>  
+      <Song />
+      <Player />
     </div>
   );
 }
